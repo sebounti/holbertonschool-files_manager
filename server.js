095ed13b77d4c695e14bf5d04078d4f7f7f2d506
lenne sebastien <sebastienlenne@gmail.com>
2024-04-03 11:07:11 +0200

    add new files for task 2

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import controllerRouting from './routes/index';
+
+const app = express();
+const port = process.env.Port || 5050;
+
+app.use(express.json());
+
+controllerRouting(app);
+
+app.listen(port, () => {
+  console.log(`server running on ${port}`);
+});
+
+export default app;
