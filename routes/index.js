d151e58d961cde90055d83492aba01430aa3df20
lenne sebastien <sebastienlenne@gmail.com>
2024-04-03 23:13:20 +0200

    add file from task 3

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,23 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 function controllerRouting(app) {
   const router = express.Router();
   app.use('/', router);
 
+  // controler App
   router.get('/status', (req, res) => {
     AppController.getStatus(req, res);
   });
 
   router.get('/stats', (req, res) => {
     AppController.getStats(req, res);
+  });
+
+  // controler  user
+  router.post('/users', (req, res) => {
+    UsersController.postNew(req, res);
   });
 }
 
